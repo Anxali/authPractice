d81b6dd2c0df938534e9f926b0076d2c5902009b
Anjali Dubey <anjalidubey021@gmail.com>
2026-06-08 21:39:36 +0530

    added base code

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+module.exports = app;
